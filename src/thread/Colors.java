8bbf488755a02dc2a51ad0472bdabf4487a35a69
cosmeticlizard <pipe.barreto07@gmail.com>
2021-05-15 22:27:09 -0500

    por alguna razón, para el cursor estar en 0,0 es igual que 1,1, lo cual cuasaba un error, se corrigio ya

--- src/thread/Colors.java
+++ src/thread/Colors.java
@@ -11,25 +11,25 @@ public class Colors extends Thread {
 		this.color=color;
 		this.sleep=sleep;
 		this.start=start;
-		current=0;
+		current=1;
 	}
 
 
 
 	@Override
 	public void run() {	
-		for (int i = 0; i < 4; i++) {
+		for (int i = 0; i < 3; i++) {
 			try {
-				Thread.sleep(sleep);
 				System.out.print("\u001b["+current+";"+start+"H"+"\u001b["+color+"m*"+"\u001b[1B"+"\u001b[1D");
 				current++;
 				if(color==33) {
 					color=34;
 				}else if(color== 34) {
 					color=31;
-				}else {
+				}else{
 					color=33;
 				}
+				Thread.sleep(sleep);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
--- src/ui/Main.java
+++ src/ui/Main.java
@@ -7,9 +7,9 @@ public class Main {
 	public static void main(String [] args) throws InterruptedException {
 		
 		System.out.print("\u001b[2J");//Clear screen
-		Colors yellow= new Colors(10,0,31);
-		Colors blue= new Colors(500,2,31);
-		Colors red= new Colors(1000,3,31);
+		Colors yellow= new Colors(1,0,33);
+		Colors blue= new Colors(100,4,33);
+		Colors red= new Colors(1000,7,33);
 		yellow.start();
 		blue.start();
 		red.start();
