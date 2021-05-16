6e764fcb2a18adc11ec669061ef71b70f7d63eab
cosmeticlizard <pipe.barreto07@gmail.com>
2021-05-15 22:17:07 -0500

    Imprime mini bandera

--- src/thread/Colors.java
+++ src/thread/Colors.java
@@ -6,22 +6,30 @@ public class Colors extends Thread {
 	private int color;
 	private int start;
 	private int current;
-	
+
 	public Colors( int sleep, int start,int color) {	
 		this.color=color;
 		this.sleep=sleep;
 		this.start=start;
 		current=0;
 	}
-	
+
+
+
 	@Override
-	public void run() {
-		
-		for (int i = 0; i < 5; i++) {
+	public void run() {	
+		for (int i = 0; i < 4; i++) {
 			try {
 				Thread.sleep(sleep);
 				System.out.print("\u001b["+current+";"+start+"H"+"\u001b["+color+"m*"+"\u001b[1B"+"\u001b[1D");
 				current++;
+				if(color==33) {
+					color=34;
+				}else if(color== 34) {
+					color=31;
+				}else {
+					color=33;
+				}
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
--- src/ui/Main.java
+++ src/ui/Main.java
@@ -7,9 +7,9 @@ public class Main {
 	public static void main(String [] args) throws InterruptedException {
 		
 		System.out.print("\u001b[2J");//Clear screen
-		Colors yellow= new Colors(0,0,33);
-		Colors blue= new Colors(500,2,34);
-		Colors red= new Colors(100,3,31);
+		Colors yellow= new Colors(10,0,31);
+		Colors blue= new Colors(500,2,31);
+		Colors red= new Colors(1000,3,31);
 		yellow.start();
 		blue.start();
 		red.start();
