83c38c54b8ab7393d686bf3efea3d4d527a87030
cosmeticlizard <pipe.barreto07@gmail.com>
2021-05-15 22:44:20 -0500

    Se cambian algunas cosas, se mueve el sleep de lugar

--- src/thread/Colors.java
+++ src/thread/Colors.java
@@ -20,6 +20,7 @@ public class Colors extends Thread {
 	public void run() {	
 		for (int i = 0; i < 3; i++) {
 			try {
+				Thread.sleep(sleep);
 				System.out.print("\u001b["+current+";"+start+"H"+"\u001b["+color+"m*"+"\u001b[1B"+"\u001b[1D");
 				current++;
 				if(color==33) {
@@ -29,7 +30,7 @@ public class Colors extends Thread {
 				}else{
 					color=33;
 				}
-				Thread.sleep(sleep);
+			
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
